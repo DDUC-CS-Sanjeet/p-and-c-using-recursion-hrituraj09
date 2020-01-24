5ae808132cf485169106e6c12f1555837a401e93
hrituraj09 <rituraajrajput0@gmail.com>
2020-01-24 22:04:41 +0530

    24-jan

--- perm_and_comb.cpp
+++ perm_and_comb.cpp
@@ -3,15 +3,58 @@ using namespace std;
 
 int permutation(int n, int r)
 {
-  return 0;
+	if(r==0)
+	{
+		return 1;
+	}
+	else
+	{
+		return n*permutation(n-1,r-1);
+	}
 }
 
 int combination(int n, int r)
-{
-  return 0;
+{ 
+	if(r==0)
+	{
+		return 1;
+	}
+	else
+	{
+		return 1.0*n*combination(n-1,r-1)/r;
+	}
 }
 
 int main()
 {
+	int x=1,n,r,p,c;
+	cout<<"Enter values of n and r respectively: "<<endl;
+	cin>>n>>r;
+	if(r<=n)
+	{
+		try
+		{
+			if(n<0||r<0)
+			{
+				throw x;
+			}
+			else if(r==0)
+			{
+				p=1;
+				c=1;
+			}
+			else
+			{
+				cout<<"Permutation :"<<permutation(n,r)<<endl;
+				cout<<"Combination :"<<combination(n,r)<<endl;
+			}
+		}
+		catch(int y)
+		{
+			cout<<"Invalid input!!";
+		}
+	}
+	else
+	cout<<"Invalid input!!"<<endl;
   return 0;
 }
